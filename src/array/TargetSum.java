c158be6c69211bae0365b98c217672040a7c4b05
Akash Raj <43521630+akashrajmuj@users.noreply.github.com>
2020-10-03 21:38:40 +0530

    Update TargetSum.java

--- src/array/TargetSum.java
+++ src/array/TargetSum.java
@@ -4,24 +4,35 @@ public class TargetSum {
 	// This algorithm is based on two pointers to get
 	// the given target sum
 	// NB: Expects a sorted input array to get the right answer
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(targetSum(new int[] {1,2,3,4,5,7,9,12,13,14,16}, 14)));
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int arr[]=new int[10],target;
+		System.out.println("Enter Ten elements in array");
+		for(int i=0;i<10;i++)
+			arr[i]=sc.nextInt();
+		System.out.println("Enter target element");
+		target=sc.nextInt();
+		System.out.println(Arrays.toString(targetSum(arr, target)));
 	}
 
 	// targetSum returns the index of the pair for the
 	// elements that add up to the target
 	public static int[] targetSum(int[] arr, int target) {
 		int start = 0, end = arr.length - 1;
-		while (start < end) {
-			int sum = (arr[start] +  arr[end]);
+		while (sum!=target && start < end) {       //improved while loop condition
+			int sum = (arr[start] + arr[end]);
 			if (sum < target) {
 				++start;
-			} else if (sum == target) {
-				return new int[] { arr[start], arr[end] };
-			} else {
+			}
+			else {
 				--end;
 			}
 		}
+		if(sum==target)
+			return new int[] { arr[start], arr[end] };
+		else
 		return new int[] {};		
 	}
 }
